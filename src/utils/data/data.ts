3f124cd4de0d9beb672c12021857c1dfcd9178e7
michaelkasingye <michaelkasingye@gmail.com>
2023-01-02 21:32:54 +0300

    add basic data structure

---
+++ src/utils/data/data.ts
@@ -0,0 +1,22 @@
+export const cars = [
+    {
+        id: 1,
+        name: 'Raum',
+        year: '2001/10',
+        engine: '1500cc',
+        maxMilage: 100000,
+        minMilage: 150000,
+        usage: 'Bond New',
+        price: 23000000,
+        description: 'lorem12',
+        images: [
+            'https://res.cloudinary.com/dgocgr4g5/image/upload/v1672562128/cars/Raum/2010%20t0%202020/BM762394_28_mebxhj.jpg',
+            'https://res.cloudinary.com/dgocgr4g5/image/upload/v1672562124/cars/Raum/2010%20t0%202020/BM762394_27_hz6mal.jpg',
+            'https://res.cloudinary.com/dgocgr4g5/image/upload/v1672562115/cars/Raum/2010%20t0%202020/BM762394_26_cpopes.jpg',
+            'https://res.cloudinary.com/dgocgr4g5/image/upload/v1672562102/cars/Raum/2010%20t0%202020/BM762394_31_rwpre8.jpg',
+            'https://res.cloudinary.com/dgocgr4g5/image/upload/v1672562081/cars/Raum/2010%20t0%202020/BM762394_4_kdvyip.jpg',
+            'https://res.cloudinary.com/dgocgr4g5/image/upload/v1672562073/cars/Raum/2010%20t0%202020/BM762394_8_j96wuh.jpg',
+            'https://res.cloudinary.com/dgocgr4g5/image/upload/v1672562064/cars/Raum/2010%20t0%202020/BM762394_6_g5knap.jpg',
+        ],
+    },
+]
